4b6dedfcbfeac169cf5896e62623e84d942d54e7
PrashantM512 <prashantmorade512@gmail.com>
2023-08-28 10:58:45 +0530

    Pushed structure

--- CURD-Operations-using-JSP-Servlet-jdbc-/src/main/java/com/prash/dao/UserDAO.java
+++ CURD-Operations-using-JSP-Servlet-jdbc-/src/main/java/com/prash/dao/UserDAO.java
@@ -9,6 +9,8 @@ public class UserDAO {
 	public static final String Insert_Users="INSERT INTO users"+"(name,email,country) VALUES "+"(?,?,?);";
     public static final String Select_Users="SELECT * FROM users";
     public static final String Delete_Users="DELETE FROM users WHERE id=?;";
-    public static final String Update_Users="UPDATE users SET name=?,email=?,country=? WHERE id=?;";
+    public static final String Update_Users="UPDATE users SET name=?,email=?,country=? WHERE id=?;"
+    		
+    		;
 }
 
